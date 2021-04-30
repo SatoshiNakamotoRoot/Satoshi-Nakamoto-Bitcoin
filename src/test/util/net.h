655a2cf6660e02b2a7bd2bf641fd11f6732dc153
Vasil Dimov <vd@FreeBSD.org>
2021-04-30 17:07:35 +0200

    fuzz: add CConnman::SocketHandler() to the tests

--- src/test/util/net.h
+++ src/test/util/net.h
@@ -75,6 +75,11 @@ struct ConnmanTestMsg : public CConnman {
         return InitBinds(options);
     }
 
+    void SocketHandlerPublic()
+    {
+        SocketHandler();
+    }
+
     void Handshake(CNode& node,
                    bool successfully_connected,
                    ServiceFlags remote_services,
