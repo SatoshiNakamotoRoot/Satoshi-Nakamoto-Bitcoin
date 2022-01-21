9b74dc0f9949e985c5533f20d8b8f7ae31e9c2b0
Ryan Ofsky <ryan@ofsky.org>
2022-01-21 12:31:23 -0500

    indexes, refactor: Add Commit CBlockLocator& argument
    This commit does not change behavior in any way.

--- src/index/base.cpp
+++ src/index/base.cpp
@@ -90,7 +90,7 @@ void BaseIndexNotifications::chainStateFlushed(ChainstateRole role, const CBlock
     // No need to handle errors in Commit. If it fails, the error will be already be logged. The
     // best way to recover is to continue, as index cannot be corrupted by a missed commit to disk
     // for an advanced index state.
-    m_index.Commit();
+    m_index.Commit(locator);
 }
 
 BaseIndex::DB::DB(const fs::path& path, size_t n_cache_size, bool f_memory, bool f_wipe, bool f_obfuscate) :
@@ -251,7 +251,7 @@ void BaseIndex::ThreadSync()
                 // No need to handle errors in Commit. If it fails, the error will be already be
                 // logged. The best way to recover is to continue, as index cannot be corrupted by
                 // a missed commit to disk for an advanced index state.
-                Commit();
+                Commit(GetLocator(*m_chain, pindex->GetBlockHash()));
                 return;
             }
 
@@ -263,7 +263,7 @@ void BaseIndex::ThreadSync()
                     m_synced = true;
                     m_chain->context()->validation_signals->CallFunctionInValidationInterfaceQueue([this] { m_ready = true; });
                     // No need to handle errors in Commit. See rationale above.
-                    Commit();
+                    Commit(GetLocator(*m_chain, pindex->GetBlockHash()));
                     break;
                 }
                 if (pindex_next->pprev != pindex && !Rewind(pindex, pindex_next->pprev)) {
@@ -285,7 +285,7 @@ void BaseIndex::ThreadSync()
                 SetBestBlockIndex(pindex->pprev);
                 last_locator_write_time = current_time;
                 // No need to handle errors in Commit. See rationale above.
-                Commit();
+                Commit(GetLocator(*m_chain, pindex->GetBlockHash()));
             }
 
             CBlock block;
@@ -312,16 +312,16 @@ void BaseIndex::ThreadSync()
     }
 }
 
-bool BaseIndex::Commit()
+bool BaseIndex::Commit(const CBlockLocator& locator)
 {
     // Don't commit anything if we haven't indexed any block yet
     // (this could happen if init is interrupted).
-    bool ok = m_best_block_index != nullptr;
+    bool ok = !locator.IsNull();
     if (ok) {
         CDBBatch batch(GetDB());
         ok = CustomCommit(batch);
         if (ok) {
-            GetDB().WriteBestBlock(batch, GetLocator(*m_chain, m_best_block_index.load()->GetBlockHash()));
+            GetDB().WriteBestBlock(batch, locator);
             ok = GetDB().WriteBatch(batch);
         }
     }
@@ -347,7 +347,7 @@ bool BaseIndex::Rewind(const CBlockIndex* current_tip, const CBlockIndex* new_ti
     // In case we reorg beyond the pruned depth, ReadBlockFromDisk would
     // throw and lead to a graceful shutdown
     SetBestBlockIndex(new_tip);
-    if (!Commit()) {
+    if (!Commit(GetLocator(*m_chain, new_tip->GetBlockHash()))) {
         // If commit fails, revert the best block index to avoid corruption.
         SetBestBlockIndex(current_tip);
         return false;
