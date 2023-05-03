ryanofsky/bitcoin

multiprocess: Add debug.log .wallet/.gui suffixes
Add .wallet/.gui suffixes to log files created by bitcoin-gui and
bitcoin-wallet processes so they don't clash with bitcoin-node log file.

test/functional/test_framework/test_node.py
@@ -527,7 +527,7 @@ class TestNode():
         # it would shut down cleanly instead of uncleanly when it lost the
         # connection to the node, which is not what we want for testing.
         wallet_pid = None
-        with open(self.debug_log_path, encoding="utf-8", errors="replace") as dl:
+        with open(self.debug_log_path(wallet=False), encoding="utf-8", errors="replace") as dl:
             for line in dl:
                 if m := re.search(r"\[.*?spawnProcess.*?\] \[ipc\] Process bitcoin-wallet pid ([0-9]+) launched", line):
                     wallet_pid = int(m.group(1))
@@ -556,9 +556,13 @@ class TestNode():
     def chain_path(self) -> Path:
         return self.datadir_path / self.chain
 
-    @property
-    def debug_log_path(self) -> Path:
-        return self.chain_path / 'debug.log'
+    def debug_log_path(self, wallet: bool) -> Path:
+        path = self.chain_path / 'debug.log'
+        if wallet:
+            wallet_path = path.with_name(path.name + ".wallet")
+            if wallet_path.exists():
+                path = wallet_path
+        return path
 
     @property
     def blocks_path(self) -> Path:
@@ -576,13 +580,13 @@ class TestNode():
     def wallets_path(self) -> Path:
         return self.chain_path / "wallets"
 
-    def debug_log_size(self, **kwargs) -> int:
-        with open(self.debug_log_path, **kwargs) as dl:
+    def debug_log_size(self, wallet: bool, **kwargs) -> int:
+        with open(self.debug_log_path(wallet), **kwargs) as dl:
             dl.seek(0, 2)
             return dl.tell()
 
     @contextlib.contextmanager
-    def assert_debug_log(self, expected_msgs, unexpected_msgs=None, *, timeout=0):
+    def assert_debug_log(self, expected_msgs, unexpected_msgs=None, *, timeout=0, wallet=False):
         if unexpected_msgs is None:
             unexpected_msgs = []
         assert_equal(type(expected_msgs), list)
@@ -590,7 +594,7 @@ class TestNode():
         remaining_expected = list(expected_msgs)
 
         time_end = time.time() + timeout * self.timeout_factor
-        prev_size = self.debug_log_size(encoding="utf-8")  # Must use same encoding that is used to read() below
+        prev_size = self.debug_log_size(wallet, encoding="utf-8")  # Must use same encoding that is used to read() below
 
         def join_log(log):
             return " - " + "\n - ".join(log.splitlines())
@@ -598,7 +602,7 @@ class TestNode():
         yield
 
         while True:
-            with open(self.debug_log_path, encoding="utf-8", errors="replace") as dl:
+            with open(self.debug_log_path(wallet), encoding="utf-8", errors="replace") as dl:
                 dl.seek(prev_size)
                 log = dl.read()
             for unexpected_msg in unexpected_msgs:
@@ -617,18 +621,18 @@ class TestNode():
                                     f'not found in log:\n\n{join_log(log)}\n\n')
 
     @contextlib.contextmanager
-    def busy_wait_for_debug_log(self, expected_msgs, timeout=60):
+    def busy_wait_for_debug_log(self, expected_msgs, timeout=60, wallet=False):
         """
         Block until we see a particular debug log message fragment or until we exceed the timeout.
         """
         time_end = time.time() + timeout * self.timeout_factor
-        prev_size = self.debug_log_size(mode="rb")  # Must use same mode that is used to read() below
+        prev_size = self.debug_log_size(wallet, mode="rb")  # Must use same mode that is used to read() below
         remaining_expected = list(expected_msgs)
 
         yield
 
         while True:
-            with open(self.debug_log_path, "rb") as dl:
+            with open(self.debug_log_path(wallet), "rb") as dl:
                 dl.seek(prev_size)
                 log = dl.read()
 

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

test/functional/test_framework/test_node.py
@@ -406,27 +406,31 @@ class TestNode():
     def chain_path(self) -> Path:
         return Path(self.datadir) / self.chain
 
-    @property
-    def debug_log_path(self) -> Path:
-        return self.chain_path / 'debug.log'
+    def debug_log_path(self, wallet: bool) -> Path:
+        path = self.chain_path / 'debug.log'
+        if wallet:
+            wallet_path = path.with_name(path.name + ".wallet")
+            if wallet_path.exists():
+                path = wallet_path
+        return path
 
-    def debug_log_bytes(self) -> int:
-        with open(self.debug_log_path, encoding='utf-8') as dl:
+    def debug_log_bytes(self, wallet: bool) -> int:
+        with open(self.debug_log_path(wallet), encoding='utf-8') as dl:
             dl.seek(0, 2)
             return dl.tell()
 
     @contextlib.contextmanager
-    def assert_debug_log(self, expected_msgs, unexpected_msgs=None, timeout=2):
+    def assert_debug_log(self, expected_msgs, unexpected_msgs=None, timeout=2, wallet=False):
         if unexpected_msgs is None:
             unexpected_msgs = []
         time_end = time.time() + timeout * self.timeout_factor
-        prev_size = self.debug_log_bytes()
+        prev_size = self.debug_log_bytes(wallet)
 
         yield
 
         while True:
             found = True
-            with open(self.debug_log_path, encoding='utf-8') as dl:
+            with open(self.debug_log_path(wallet), encoding='utf-8') as dl:
                 dl.seek(prev_size)
                 log = dl.read()
             print_log = " - " + "\n - ".join(log.splitlines())
@@ -444,20 +448,20 @@ class TestNode():
         self._raise_assertion_error('Expected messages "{}" does not partially match log:\n\n{}\n\n'.format(str(expected_msgs), print_log))
 
     @contextlib.contextmanager
-    def wait_for_debug_log(self, expected_msgs, timeout=60):
+    def wait_for_debug_log(self, expected_msgs, timeout=60, wallet=False):
         """
         Block until we see a particular debug log message fragment or until we exceed the timeout.
         Return:
             the number of log lines we encountered when matching
         """
         time_end = time.time() + timeout * self.timeout_factor
-        prev_size = self.debug_log_bytes()
+        prev_size = self.debug_log_bytes(wallet)
 
         yield
 
         while True:
             found = True
-            with open(self.debug_log_path, "rb") as dl:
+            with open(self.debug_log_path(wallet), "rb") as dl:
                 dl.seek(prev_size)
                 log = dl.read()
 