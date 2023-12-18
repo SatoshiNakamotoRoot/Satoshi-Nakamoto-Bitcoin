bitcoin/bitcoin

[txorphanage] GetAllWtxids

src/txorphanage.cpp
@@ -348,3 +348,14 @@ std::optional<std::vector<uint256>> TxOrphanage::GetParentTxids(const uint256& w
     if (it != m_wtxid_to_orphan_it.end()) return it->second->second.parent_txids;
     return std::nullopt;
 }
+
+std::vector<uint256> TxOrphanage::GetAllWtxids() const
+{
+    AssertLockNotHeld(m_mutex);
+    LOCK(m_mutex);
+    std::vector<uint256> wtxids;
+    wtxids.reserve(m_wtxid_to_orphan_it.size());
+    std::transform(m_wtxid_to_orphan_it.cbegin(), m_wtxid_to_orphan_it.cend(), std::back_inserter(wtxids),
+            [](const auto& kv) { return kv.first; });
+    return wtxids;
+}

src/txorphanage.h
@@ -98,6 +98,9 @@ public:
     /** Get an orphan's parent_txids, or std::nullopt if the orphan is not present. */
     std::optional<std::vector<uint256>> GetParentTxids(const uint256& wtxid) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
 
+    /** Get wtxids of all orphan transactions. */
+    std::vector<uint256> GetAllWtxids() const EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+
 protected:
     /** Total bytes of all transactions. */
     unsigned int m_total_orphan_bytes{0};

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

src/txorphanage.cpp
@@ -303,3 +303,14 @@ std::optional<std::vector<Txid>> TxOrphanage::GetParentTxids(const Wtxid& wtxid)
     if (it != m_wtxid_to_orphan_it.end()) return it->second->second.parent_txids;
     return std::nullopt;
 }
+
+std::vector<Wtxid> TxOrphanage::GetAllWtxids() const
+{
+    AssertLockNotHeld(m_mutex);
+    LOCK(m_mutex);
+    std::vector<Wtxid> wtxids;
+    wtxids.reserve(m_wtxid_to_orphan_it.size());
+    std::transform(m_wtxid_to_orphan_it.cbegin(), m_wtxid_to_orphan_it.cend(), std::back_inserter(wtxids),
+            [](const auto& kv) { return kv.first; });
+    return wtxids;
+}

src/txorphanage.h
@@ -89,6 +89,9 @@ public:
         return peer_bytes_it == m_peer_bytes_used.end() ? 0 : peer_bytes_it->second;
     }
 
+    /** Get wtxids of all orphan transactions. */
+    std::vector<Wtxid> GetAllWtxids() const EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+
 protected:
     /** Total bytes of all transactions. */
     unsigned int m_total_orphan_bytes{0};