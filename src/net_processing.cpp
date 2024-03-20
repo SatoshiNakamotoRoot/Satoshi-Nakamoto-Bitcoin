bba9e649d53dfdf9c4063d1f15427929f4d66827
Ryan Ofsky <ryan@ofsky.org>
2024-03-20 17:12:09 -0400

    refactor, validation: Return fatal errors from new block functions
    Return fatal errors from AcceptBlock, ProcessNewBlock, TestBlockValidity,
    LoadGenesisBlock, and LoadExternalBlockFile. Also add nodiscard annotations so
    callers handle the result values.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -54,6 +54,9 @@
 #include <typeinfo>
 #include <utility>
 
+using kernel::AbortFailure;
+using kernel::FlushResult;
+
 /** Headers download timeout.
  *  Timeout = base + per_header * (expected number of headers) */
 static constexpr auto HEADERS_DOWNLOAD_TIMEOUT_BASE = 15min;
@@ -3555,7 +3558,8 @@ void PeerManagerImpl::ProcessGetCFCheckPt(CNode& node, Peer& peer, DataStream& v
 void PeerManagerImpl::ProcessBlock(CNode& node, const std::shared_ptr<const CBlock>& block, bool force_processing, bool min_pow_checked)
 {
     bool new_block{false};
-    m_chainman.ProcessNewBlock(block, force_processing, min_pow_checked, &new_block);
+    FlushResult<void, AbortFailure> process_result;
+    (void)m_chainman.ProcessNewBlock(block, force_processing, min_pow_checked, &new_block, process_result);
     if (new_block) {
         node.m_last_block_time = GetTime<std::chrono::seconds>();
         // In case this block came from a different peer than we requested
--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -64,7 +64,9 @@
 #include <functional>
 #include <stdexcept>
 
+using kernel::AbortFailure;
 using kernel::BlockTreeDB;
+using kernel::FlushResult;
 using node::ApplyArgsManOptions;
 using node::BlockAssembler;
 using node::BlockManager;
@@ -394,7 +396,9 @@ CBlock TestChain100Setup::CreateAndProcessBlock(
 
     CBlock block = this->CreateBlock(txns, scriptPubKey, *chainstate);
     std::shared_ptr<const CBlock> shared_pblock = std::make_shared<const CBlock>(block);
-    Assert(m_node.chainman)->ProcessNewBlock(shared_pblock, true, true, nullptr);
+    FlushResult<void, AbortFailure> process_result;
+    (void)Assert(m_node.chainman)->ProcessNewBlock(shared_pblock, true, true, nullptr, process_result);
+    Assert(process_result);
 
     return block;
 }
