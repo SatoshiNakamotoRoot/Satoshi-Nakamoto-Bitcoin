ea187ec73ab4140310fa04aa8ee4f40402b681ca
fanquake <fanquake@gmail.com>
2024-04-15 13:54:46 +0100

    depends: expat 2.6.2

--- depends/packages/expat.mk
+++ depends/packages/expat.mk
@@ -1,8 +1,8 @@
 package=expat
-$(package)_version=2.4.8
+$(package)_version=2.6.2
 $(package)_download_path=https://github.com/libexpat/libexpat/releases/download/R_$(subst .,_,$($(package)_version))/
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=f79b8f904b749e3e0d20afeadecf8249c55b2e32d4ebb089ae378df479dcaf25
+$(package)_sha256_hash=ee14b4c5d8908b1bec37ad937607eab183d4d9806a08adee472c3c3121d27364
 
 # -D_DEFAULT_SOURCE defines __USE_MISC, which exposes additional
 # definitions in endian.h, which are required for a working
