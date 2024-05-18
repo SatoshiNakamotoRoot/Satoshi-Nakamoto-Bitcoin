b382bf4755e2b503cb1ee2f87a7b0f66dc5b76f4
naiyoma <lankas.aurelia@gmail.com>
2024-05-18 23:16:31 +0300

    common/args.h: separate out cli commands
    Co-authored-by: Anthony Towns <aj@erisian.com.au>

--- src/common/args.cpp
+++ src/common/args.cpp
@@ -634,6 +634,9 @@ std::string ArgsManager::GetHelpMessage() const
             case OptionsCategory::REGISTER_COMMANDS:
                 usage += HelpMessageGroup("Register Commands:");
                 break;
+            case OptionsCategory::CLI_COMMANDS:
+                usage += HelpMessageGroup("Cli Commands:");
+                break;
             default:
                 break;
         }
