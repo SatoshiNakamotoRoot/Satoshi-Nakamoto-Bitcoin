ba6bc110441a40c8758b3bdbed3b89a450ac9b5e
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2024-05-20 20:57:19 +0100

    cmake: Add Python-based tests

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -2,6 +2,18 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit/.
 
+if(TARGET bitcoin-util AND TARGET bitcoin-tx AND PYTHON_COMMAND)
+  add_test(NAME util_test_runner
+    COMMAND ${CMAKE_COMMAND} -E env BITCOINUTIL=$<TARGET_FILE:bitcoin-util> BITCOINTX=$<TARGET_FILE:bitcoin-tx> ${PYTHON_COMMAND} ${CMAKE_BINARY_DIR}/test/util/test_runner.py
+  )
+endif()
+
+if(PYTHON_COMMAND)
+  add_test(NAME util_rpcauth_test
+    COMMAND ${PYTHON_COMMAND} ${CMAKE_BINARY_DIR}/test/util/rpcauth-test.py
+  )
+endif()
+
 if(TARGET bench_bitcoin)
   add_test(NAME bench_sanity_check_high_priority
     COMMAND bench_bitcoin -sanity-check -priority-level=high
