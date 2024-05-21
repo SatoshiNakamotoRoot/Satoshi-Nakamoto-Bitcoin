84e03ab48e0c832ab2c0d8e6babb6563a0ee92dc
Kristaps Kaupe <kristaps@blogiem.lv>
2024-05-21 04:31:13 +0300

    Test that permitbaremultisig is enabled by default

--- test/functional/mempool_datacarrier.py
+++ test/functional/mempool_datacarrier.py
@@ -51,6 +51,9 @@ class DataCarrierTest(BitcoinTestFramework):
         assert_equal(self.nodes[2].getmempoolinfo()["maxdatacarriersize"], MAX_OP_RETURN_RELAY - 1)
         assert_equal(self.nodes[3].getmempoolinfo()["maxdatacarriersize"], 2)
 
+        # Test that bare multisig is allowed by default.
+        assert_equal(self.nodes[0].getmempoolinfo()["permitbaremultisig"], True)
+
         # By default, only 80 bytes are used for data (+1 for OP_RETURN, +2 for the pushdata opcodes).
         default_size_data = randbytes(MAX_OP_RETURN_RELAY - 3)
         too_long_data = randbytes(MAX_OP_RETURN_RELAY - 2)
