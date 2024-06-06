bitcoin/bitcoin

wallet: Get the depth in main chain for a TxState

src/wallet/wallet.cpp
@@ -3392,18 +3392,24 @@ CKeyPool::CKeyPool(const CPubKey& vchPubKeyIn, bool internalIn)
     m_pre_split = false;
 }
 
-int CWallet::GetTxDepthInMainChain(const CWalletTx& wtx) const
+int CWallet::GetTxStateDepthInMainChain(const TxState& state) const
 {
     AssertLockHeld(cs_wallet);
-    if (auto* conf = wtx.state<TxStateConfirmed>()) {
+    if (auto* conf = std::get_if<TxStateConfirmed>(&state)) {
         assert(conf->confirmed_block_height >= 0);
         return GetLastBlockHeight() - conf->confirmed_block_height + 1;
-    } else if (auto* conf = wtx.state<TxStateConflicted>()) {
+    } else if (auto* conf = std::get_if<TxStateConflicted>(&state)) {
         assert(conf->conflicting_block_height >= 0);
         return -1 * (GetLastBlockHeight() - conf->conflicting_block_height + 1);
     } else {
         return 0;
     }
+}
+
+int CWallet::GetTxDepthInMainChain(const CWalletTx& wtx) const
+{
+    AssertLockHeld(cs_wallet);
+    return GetTxStateDepthInMainChain(wtx.m_state);
 }
 
 int CWallet::GetTxBlocksToMaturity(const CWalletTx& wtx) const

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

src/wallet/wallet.cpp
@@ -3429,18 +3429,24 @@ CKeyPool::CKeyPool(const CPubKey& vchPubKeyIn, bool internalIn)
     m_pre_split = false;
 }
 
-int CWallet::GetTxDepthInMainChain(const CWalletTx& wtx) const
+int CWallet::GetTxStateDepthInMainChain(const TxState& state) const
 {
     AssertLockHeld(cs_wallet);
-    if (auto* conf = wtx.state<TxStateConfirmed>()) {
+    if (auto* conf = std::get_if<TxStateConfirmed>(&state)) {
         assert(conf->confirmed_block_height >= 0);
         return GetLastBlockHeight() - conf->confirmed_block_height + 1;
-    } else if (auto* conf = wtx.state<TxStateBlockConflicted>()) {
+    } else if (auto* conf = std::get_if<TxStateBlockConflicted>(&state)) {
         assert(conf->conflicting_block_height >= 0);
         return -1 * (GetLastBlockHeight() - conf->conflicting_block_height + 1);
     } else {
         return 0;
     }
+}
+
+int CWallet::GetTxDepthInMainChain(const CWalletTx& wtx) const
+{
+    AssertLockHeld(cs_wallet);
+    return GetTxStateDepthInMainChain(wtx.m_state);
 }
 
 int CWallet::GetTxBlocksToMaturity(const CWalletTx& wtx) const