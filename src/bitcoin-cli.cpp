bitcoin/bitcoin

net: introduce a new connection type for private broadcast
We will open a short-lived connection to a random Tor or I2P peer,
send our transaction to that peer and close the connection.

src/bitcoin-cli.cpp
@@ -452,6 +452,7 @@ private:
         if (conn_type == "block-relay-only") return "block";
         if (conn_type == "manual" || conn_type == "feeler") return conn_type;
         if (conn_type == "addr-fetch") return "addr";
+        if (conn_type == "private-broadcast") return "privat";
         return "";
     }
 
@@ -655,11 +656,12 @@ public:
         "           \"in\"  - inbound connections are those initiated by the peer\n"
         "           \"out\" - outbound connections are those initiated by us\n"
         "  type     Type of peer connection\n"
-        "           \"full\"   - full relay, the default\n"
-        "           \"block\"  - block relay; like full relay but does not relay transactions or addresses\n"
-        "           \"manual\" - peer we manually added using RPC addnode or the -addnode/-connect config options\n"
-        "           \"feeler\" - short-lived connection for testing addresses\n"
-        "           \"addr\"   - address fetch; short-lived connection for requesting addresses\n"
+        "           \"full\"      - full relay, the default\n"
+        "           \"block\"     - block relay; like full relay but does not relay transactions or addresses\n"
+        "           \"manual\"    - peer we manually added using RPC addnode or the -addnode/-connect config options\n"
+        "           \"feeler\"    - short-lived connection for testing addresses\n"
+        "           \"addr\"      - address fetch; short-lived connection for requesting addresses\n"
+        "           \"privat\"    - private broadcast; short-lived connection for broadcasting our transactions\n"
         "  net      Network the peer connected through (\"ipv4\", \"ipv6\", \"onion\", \"i2p\", \"cjdns\", or \"npr\" (not publicly routable))\n"
         "  v        Version of transport protocol used for the connection\n"
         "  mping    Minimum observed ping time, in milliseconds (ms)\n"

src/qt/rpcconsole.cpp
@@ -514,7 +514,10 @@ RPCConsole::RPCConsole(interfaces::Node& node, const PlatformStyle *_platformSty
         tr("Outbound Feeler: short-lived, for testing addresses"),
         /*: Explanatory text for a short-lived outbound peer connection that is used
             to request addresses from a peer. */
-        tr("Outbound Address Fetch: short-lived, for soliciting addresses")};
+        tr("Outbound Address Fetch: short-lived, for soliciting addresses"),
+        /*: Explanatory text for a short-lived outbound peer connection that is used
+            to broadcast privacy-sensitve data (like our transactions). */
+        tr("Private broadcast: short-lived, for broadcasting privacy-sensitve data")};
     const QString connection_types_list{"<ul><li>" + Join(CONNECTION_TYPE_DOC, QString("</li><li>")) + "</li></ul>"};
     ui->peerConnectionTypeLabel->setToolTip(ui->peerConnectionTypeLabel->toolTip().arg(connection_types_list));
     const std::vector<QString> TRANSPORT_TYPE_DOC{

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

src/bitcoin-cli.cpp
@@ -451,6 +451,7 @@ private:
         if (conn_type == "block-relay-only") return "block";
         if (conn_type == "manual" || conn_type == "feeler") return conn_type;
         if (conn_type == "addr-fetch") return "addr";
+        if (conn_type == "private-broadcast") return "priv";
         return "";
     }
 
@@ -654,11 +655,12 @@ public:
         "           \"in\"  - inbound connections are those initiated by the peer\n"
         "           \"out\" - outbound connections are those initiated by us\n"
         "  type     Type of peer connection\n"
-        "           \"full\"   - full relay, the default\n"
-        "           \"block\"  - block relay; like full relay but does not relay transactions or addresses\n"
-        "           \"manual\" - peer we manually added using RPC addnode or the -addnode/-connect config options\n"
-        "           \"feeler\" - short-lived connection for testing addresses\n"
-        "           \"addr\"   - address fetch; short-lived connection for requesting addresses\n"
+        "           \"full\"      - full relay, the default\n"
+        "           \"block\"     - block relay; like full relay but does not relay transactions or addresses\n"
+        "           \"manual\"    - peer we manually added using RPC addnode or the -addnode/-connect config options\n"
+        "           \"feeler\"    - short-lived connection for testing addresses\n"
+        "           \"addr\"      - address fetch; short-lived connection for requesting addresses\n"
+        "           \"privat\"    - private broadcast; short-lived connection for broadcasting our transactions\n"
         "  net      Network the peer connected through (\"ipv4\", \"ipv6\", \"onion\", \"i2p\", \"cjdns\", or \"npr\" (not publicly routable))\n"
         "  v        Version of transport protocol used for the connection\n"
         "  mping    Minimum observed ping time, in milliseconds (ms)\n"

src/qt/rpcconsole.cpp
@@ -515,7 +515,10 @@ RPCConsole::RPCConsole(interfaces::Node& node, const PlatformStyle *_platformSty
         tr("Outbound Feeler: short-lived, for testing addresses"),
         /*: Explanatory text for a short-lived outbound peer connection that is used
             to request addresses from a peer. */
-        tr("Outbound Address Fetch: short-lived, for soliciting addresses")};
+        tr("Outbound Address Fetch: short-lived, for soliciting addresses"),
+        /*: Explanatory text for a short-lived outbound peer connection that is used
+            to broadcast privacy-sensitive data (like our transactions). */
+        tr("Private broadcast: short-lived, for broadcasting privacy-sensitive data")};
     const QString connection_types_list{"<ul><li>" + Join(CONNECTION_TYPE_DOC, QString("</li><li>")) + "</li></ul>"};
     ui->peerConnectionTypeLabel->setToolTip(ui->peerConnectionTypeLabel->toolTip().arg(connection_types_list));
     const std::vector<QString> TRANSPORT_TYPE_DOC{