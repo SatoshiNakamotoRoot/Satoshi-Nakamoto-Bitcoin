willcl-ark/bitcoin

[rpc]: create an rpc `estimatefee`
- Given a confirmation target, we use fee estimator module that call all
available fee estimator forcasters and return the lowest fee rate that if
a transaction use will likely confirm in a given confirmation target.

Co-authored-by: willcl-ark <will@256k1.dev>

src/rpc/fees.cpp
@@ -6,8 +6,11 @@
 #include <common/messages.h>
 #include <core_io.h>
 #include <node/context.h>
+#include <policy/fee_estimator.h>
 #include <policy/feerate.h>
 #include <policy/fees.h>
+#include <policy/fees_util.h>
+#include <policy/forecaster.h>
 #include <rpc/protocol.h>
 #include <rpc/request.h>
 #include <rpc/server.h>
@@ -94,6 +97,55 @@ static RPCHelpMan estimatesmartfee()
                 result.pushKV("errors", std::move(errors));
             }
             result.pushKV("blocks", feeCalc.returnedTarget);
+            return result;
+        },
+    };
+}
+
+static RPCHelpMan estimatefee()
+{
+    return RPCHelpMan{
+        "estimatefee",
+        "\nEstimates the approximate fee per kilobyte needed for a transaction to begin\n"
+        "confirmation within conf_target blocks if possible Uses virtual transaction size as defined\n"
+        "in BIP 141 (witness data is discounted).\n",
+        {
+            {"conf_target", RPCArg::Type::NUM, RPCArg::Optional::NO, "Confirmation target in blocks"},
+        },
+        RPCResult{
+            RPCResult::Type::OBJ, "", "", {
+                {RPCResult::Type::NUM, "low", /*optional=*/true, "fee rate estimate in " + CURRENCY_UNIT + "/kvB for economical users"},
+                {RPCResult::Type::NUM, "high", /*optional=*/true, "fee rate estimate in " + CURRENCY_UNIT + "/kvB for conservative users"},
+                {RPCResult::Type::STR, "forecaster", /*optional=*/true, "the forecaster that provide the fee rate estimate"},
+                {RPCResult::Type::ARR, "errors", /*optional=*/true, "Errors encountered during processing (if there are any)", {
+                    {RPCResult::Type::STR, "", "error"},
+                }},
+            }},
+        RPCExamples{HelpExampleCli("estimatefee", "2") + HelpExampleRpc("estimatefee", "2")},
+        [&](const RPCHelpMan& self, const JSONRPCRequest& request) -> UniValue {
+            FeeEstimator& fee_estimator = EnsureAnyFeeForecasters(request.context);
+            const unsigned int targetBlocks = request.params[0].getInt<unsigned int>();
+            UniValue result(UniValue::VOBJ);
+            UniValue errors(UniValue::VARR);
+            if (targetBlocks <= 0) {
+                errors.push_back("confirmation target must be greater than 0");
+                result.pushKV("errors", errors);
+                return result;
+            }
+            std::pair<ForecastResult, std::vector<std::string>> forecast_result = fee_estimator.GetFeeEstimateFromForecasters(/*targetBlocks=*/targetBlocks);
+
+            if (!forecast_result.first.empty()) {
+                result.pushKV("low", ValueFromAmount(forecast_result.first.m_forecast_opt.m_l_priority_estimate.GetFeePerK()));
+                result.pushKV("high", ValueFromAmount(forecast_result.first.m_forecast_opt.m_h_priority_estimate.GetFeePerK()));
+                result.pushKV("forecaster", forecastTypeToString(forecast_result.first.m_forecast_opt.m_forecaster));
+            } else {
+                errors.push_back("Failed to get estimate from forecasters");
+            }
+
+            for (auto& err : forecast_result.second) {
+                errors.push_back(err);
+            }
+            result.pushKV("errors", errors);
             return result;
         },
     };
@@ -223,6 +275,7 @@ void RegisterFeeRPCCommands(CRPCTable& t)
 {
     static const CRPCCommand commands[]{
         {"util", &estimatesmartfee},
+        {"util", &estimatefee},
         {"hidden", &estimaterawfee},
     };
     for (const auto& c : commands) {

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

src/rpc/fees.cpp
@@ -6,8 +6,11 @@
 #include <common/messages.h>
 #include <core_io.h>
 #include <node/context.h>
+#include <policy/fee_estimator.h>
 #include <policy/feerate.h>
 #include <policy/fees.h>
+#include <policy/forecaster.h>
+#include <policy/forecaster_util.h>
 #include <rpc/protocol.h>
 #include <rpc/request.h>
 #include <rpc/server.h>
@@ -94,6 +97,55 @@ static RPCHelpMan estimatesmartfee()
                 result.pushKV("errors", std::move(errors));
             }
             result.pushKV("blocks", feeCalc.returnedTarget);
+            return result;
+        },
+    };
+}
+
+static RPCHelpMan estimatefee()
+{
+    return RPCHelpMan{
+        "estimatefee",
+        "\nEstimates the approximate fee per kilobyte needed for a transaction to begin\n"
+        "confirmation within conf_target blocks if possible Uses virtual transaction size as defined\n"
+        "in BIP 141 (witness data is discounted).\n",
+        {
+            {"conf_target", RPCArg::Type::NUM, RPCArg::Optional::NO, "Confirmation target in blocks"},
+        },
+        RPCResult{
+            RPCResult::Type::OBJ, "", "", {
+                {RPCResult::Type::NUM, "low", /*optional=*/true, "fee rate estimate in " + CURRENCY_UNIT + "/kvB for economical users"},
+                {RPCResult::Type::NUM, "high", /*optional=*/true, "fee rate estimate in " + CURRENCY_UNIT + "/kvB for conservative users"},
+                {RPCResult::Type::STR, "forecaster", /*optional=*/true, "the forecaster that provide the fee rate estimate"},
+                {RPCResult::Type::ARR, "errors", /*optional=*/true, "Errors encountered during processing (if there are any)", {
+                    {RPCResult::Type::STR, "", "error"},
+                }},
+            }},
+        RPCExamples{HelpExampleCli("estimatefee", "2") + HelpExampleRpc("estimatefee", "2")},
+        [&](const RPCHelpMan& self, const JSONRPCRequest& request) -> UniValue {
+            FeeEstimator& fee_estimator = EnsureAnyFeeForecasters(request.context);
+            const unsigned int targetBlocks = request.params[0].getInt<unsigned int>();
+            UniValue result(UniValue::VOBJ);
+            UniValue errors(UniValue::VARR);
+            if (targetBlocks <= 0) {
+                errors.push_back("confirmation target must be greater than 0");
+                result.pushKV("errors", errors);
+                return result;
+            }
+            std::pair<ForecastResult, std::vector<std::string>> forecast_result = fee_estimator.GetFeeEstimateFromForecasters(/*targetBlocks=*/targetBlocks);
+
+            if (!forecast_result.first.empty()) {
+                result.pushKV("low", ValueFromAmount(forecast_result.first.opt.low_priority_estimate.GetFeePerK()));
+                result.pushKV("high", ValueFromAmount(forecast_result.first.opt.high_priority_estimate.GetFeePerK()));
+                result.pushKV("forecaster", forecastTypeToString(forecast_result.first.opt.forecaster));
+            } else {
+                errors.push_back("Failed to get estimate from forecasters");
+            }
+
+            for (auto& err : forecast_result.second) {
+                errors.push_back(err);
+            }
+            result.pushKV("errors", errors);
             return result;
         },
     };
@@ -223,6 +275,7 @@ void RegisterFeeRPCCommands(CRPCTable& t)
 {
     static const CRPCCommand commands[]{
         {"util", &estimatesmartfee},
+        {"util", &estimatefee},
         {"hidden", &estimaterawfee},
     };
     for (const auto& c : commands) {