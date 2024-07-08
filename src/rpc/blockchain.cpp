dd3744365366d1c6d3230f0851c7554bc667f4c8
Sjors Provoost <sjors@sprovoost.nl>
2024-07-08 18:39:31 +0200

    rpc: check for negative timeout arg in waitfor*

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -275,6 +275,7 @@ static RPCHelpMan waitfornewblock()
     int timeout = 0;
     if (!request.params[0].isNull())
         timeout = request.params[0].getInt<int>();
+    if (timeout < 0) throw JSONRPCError(RPC_MISC_ERROR, "Negative timeout");
 
     CUpdatedBlock block;
     {
@@ -321,6 +322,7 @@ static RPCHelpMan waitforblock()
 
     if (!request.params[1].isNull())
         timeout = request.params[1].getInt<int>();
+    if (timeout < 0) throw JSONRPCError(RPC_MISC_ERROR, "Negative timeout");
 
     CUpdatedBlock block;
     {
@@ -368,6 +370,7 @@ static RPCHelpMan waitforblockheight()
 
     if (!request.params[1].isNull())
         timeout = request.params[1].getInt<int>();
+    if (timeout < 0) throw JSONRPCError(RPC_MISC_ERROR, "Negative timeout");
 
     CUpdatedBlock block;
     {
