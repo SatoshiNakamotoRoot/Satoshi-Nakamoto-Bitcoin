willcl-ark/bitcoin

[tracing]: add fee estimator tracing

src/policy/fee_estimator.cpp
@@ -68,6 +68,12 @@ std::pair<ForecastResult, std::vector<std::string>> FeeEstimator::GetFeeEstimate
                  forecastTypeToString(forecast.m_forecast_opt.m_forecaster), forecast.m_forecast_opt.m_block_height, forecast.m_forecast_opt.m_l_priority_estimate.GetFeePerK(),
                  CURRENCY_ATOM, forecast.m_forecast_opt.m_h_priority_estimate.GetFeePerK(), CURRENCY_ATOM);
 
+        TRACE5(fee_estimator, estimate_calculated,
+               targetBlocks,
+               forecastTypeToString(forecast.m_forecast_opt.m_forecaster).c_str(),
+               forecast.m_forecast_opt.m_block_height,
+               forecast.m_forecast_opt.m_l_priority_estimate.GetFeePerK(),
+               forecast.m_forecast_opt.m_h_priority_estimate.GetFeePerK());
     }
     return std::make_pair(forecast, err_messages);
 };

src/policy/forecasters/mempool.cpp
@@ -43,6 +43,15 @@ ForecastResult MemPoolForecaster::EstimateFee(unsigned int targetBlocks)
              forecastTypeToString(m_forecastType), forecast_options.m_block_height, fee_rate_estimate_result.p75.GetFeePerK(), CURRENCY_ATOM, fee_rate_estimate_result.p50.GetFeePerK(), CURRENCY_ATOM,
              fee_rate_estimate_result.p25.GetFeePerK(), CURRENCY_ATOM, fee_rate_estimate_result.p5.GetFeePerK(), CURRENCY_ATOM);
 
+    TRACE7(feerate_forecast, forecast_generated,
+           targetBlocks,
+           forecast_options.m_block_height,
+           forecastTypeToString(m_forecastType).c_str(),
+           fee_rate_estimate_result.p5.GetFeePerK(),
+           fee_rate_estimate_result.p25.GetFeePerK(),
+           fee_rate_estimate_result.p50.GetFeePerK(),
+           fee_rate_estimate_result.p75.GetFeePerK());
+
     cache.update(fee_rate_estimate_result);
     forecast_options.m_l_priority_estimate = fee_rate_estimate_result.p25;
     forecast_options.m_h_priority_estimate = fee_rate_estimate_result.p50;

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

src/policy/fee_estimator.cpp
@@ -9,6 +9,7 @@
 #include <policy/forecaster.h>
 #include <policy/forecaster_util.h>
 #include <txmempool.h>
+#include <util/trace.h>
 
 
 void FeeEstimator::RegisterForecaster(std::shared_ptr<Forecaster> forecaster)
@@ -60,6 +61,12 @@ std::pair<ForecastResult, std::vector<std::string>> FeeEstimator::GetFeeEstimate
         LogPrint(BCLog::ESTIMATEFEE, "FeeEst %s: Block height %s, low priority feerate %s %s/kvB, high priority feerate %s %s/kvB.\n",
                  forecastTypeToString(forecast.opt.forecaster), forecast.opt.block_height, forecast.opt.low_priority_estimate.GetFeePerK(),
                  CURRENCY_ATOM, forecast.opt.high_priority_estimate.GetFeePerK(), CURRENCY_ATOM);
+        TRACE5(fee_estimator, estimate_calculated,
+               targetBlocks,
+               forecastTypeToString(forecast.opt.forecaster).c_str(),
+               forecast.opt.block_height,
+               forecast.opt.low_priority_estimate.GetFeePerK(),
+               forecast.opt.high_priority_estimate.GetFeePerK());
     }
     return std::make_pair(forecast, err_messages);
 };

src/policy/forecasters/mempool.cpp
@@ -6,6 +6,7 @@
 #include <node/miner.h>
 #include <policy/fee_estimator.h>
 #include <policy/forecasters/mempool.h>
+#include <util/trace.h>
 #include <validation.h>
 
 
@@ -41,6 +42,15 @@ ForecastResult MemPoolForecaster::EstimateFee(unsigned int targetBlocks)
     LogPrint(BCLog::ESTIMATEFEE, "FeeEst: %s: Block height %s, 75th percentile feerate %s %s/kvB, 50th percentile feerate %s %s/kvB, 25th percentile feerate %s %s/kvB, 5th percentile feerate %s %s/kvB \n",
              forecastTypeToString(m_forecastType), forecast_options.block_height, fee_rate_estimate_result.p75.GetFeePerK(), CURRENCY_ATOM, fee_rate_estimate_result.p50.GetFeePerK(), CURRENCY_ATOM,
              fee_rate_estimate_result.p25.GetFeePerK(), CURRENCY_ATOM, fee_rate_estimate_result.p5.GetFeePerK(), CURRENCY_ATOM);
+
+    TRACE7(feerate_forecast, forecast_generated,
+           targetBlocks,
+           forecast_options.block_height,
+           forecastTypeToString(m_forecastType).c_str(),
+           fee_rate_estimate_result.p5.GetFeePerK(),
+           fee_rate_estimate_result.p25.GetFeePerK(),
+           fee_rate_estimate_result.p50.GetFeePerK(),
+           fee_rate_estimate_result.p75.GetFeePerK());
 
     cache.update(fee_rate_estimate_result);
     forecast_options.low_priority_estimate = fee_rate_estimate_result.p25;