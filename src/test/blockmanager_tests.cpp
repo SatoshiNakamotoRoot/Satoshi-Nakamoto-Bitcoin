ryanofsky/bitcoin

refactor: Pass Logger instances to kernel objects
Pass Logger instances to BlockManager, CCoinsViewDB, CDBWrapper,
ChainstateManager, and CoinsViews instances so libbitcoinkernel applications
and test code have the option to control where log output goes instead of
having all output sent to the global logger.

This commit just passes the logger objects without using them. The next commit
updates log print statements to use the new objects.

src/test/blockmanager_tests.cpp
@@ -40,7 +40,7 @@ BOOST_AUTO_TEST_CASE(blockmanager_find_block_pos)
             .cache_bytes = 0,
         },
     };
-    BlockManager blockman{*Assert(m_node.shutdown_signal), blockman_opts};
+    BlockManager blockman{m_logger, *Assert(m_node.shutdown_signal), blockman_opts};
     // simulate adding a genesis block normally
     LOCK(::cs_main);
     BOOST_CHECK_EQUAL(blockman.WriteBlock(params->GenesisBlock(), 0).nPos, STORAGE_HEADER_BYTES);
@@ -244,7 +244,7 @@ BOOST_AUTO_TEST_CASE(blockmanager_flush_block_file)
             .cache_bytes = 0,
         },
     };
-    BlockManager blockman{*Assert(m_node.shutdown_signal), blockman_opts};
+    BlockManager blockman{m_logger, *Assert(m_node.shutdown_signal), blockman_opts};
 
     // Test blocks with no transactions, not even a coinbase
     CBlock block1;

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

src/test/blockmanager_tests.cpp
@@ -33,7 +33,7 @@ BOOST_AUTO_TEST_CASE(blockmanager_find_block_pos)
         .blocks_dir = m_args.GetBlocksDirPath(),
         .notifications = notifications,
     };
-    BlockManager blockman{*Assert(m_node.shutdown), blockman_opts};
+    BlockManager blockman{m_logger, *Assert(m_node.shutdown), blockman_opts};
     // simulate adding a genesis block normally
     BOOST_CHECK_EQUAL(blockman.SaveBlockToDisk(params->GenesisBlock(), 0).nPos, BLOCK_SERIALIZATION_HEADER_SIZE);
     // simulate what happens during reindex
@@ -140,7 +140,7 @@ BOOST_AUTO_TEST_CASE(blockmanager_flush_block_file)
         .blocks_dir = m_args.GetBlocksDirPath(),
         .notifications = notifications,
     };
-    BlockManager blockman{*Assert(m_node.shutdown), blockman_opts};
+    BlockManager blockman{m_logger, *Assert(m_node.shutdown), blockman_opts};
 
     // Test blocks with no transactions, not even a coinbase
     CBlock block1;