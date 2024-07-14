bitcoin/bitcoin

wallet: provide WalletBatch to 'DeleteRecords'
So it can be used within an external db txn context.

src/wallet/scriptpubkeyman.cpp
@@ -2010,8 +2010,14 @@ std::optional<MigrationData> LegacyScriptPubKeyMan::MigrateToDescriptor()
 
 bool LegacyScriptPubKeyMan::DeleteRecords()
 {
+    return RunWithinTxn(m_storage.GetDatabase(), /*process_desc=*/"delete legacy records", [&](WalletBatch& batch){
+        return DeleteRecords(batch);
+    });
+}
+
+bool LegacyScriptPubKeyMan::DeleteRecords(WalletBatch& batch)
+{
     LOCK(cs_KeyStore);
-    WalletBatch batch(m_storage.GetDatabase());
     return batch.EraseRecords(DBKeys::LEGACY_TYPES);
 }
 

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

src/wallet/scriptpubkeyman.cpp
@@ -2034,8 +2034,14 @@ std::optional<MigrationData> LegacyDataSPKM::MigrateToDescriptor()
 
 bool LegacyDataSPKM::DeleteRecords()
 {
+    return RunWithinTxn(m_storage.GetDatabase(), /*process_desc=*/"delete legacy records", [&](WalletBatch& batch){
+        return DeleteRecords(batch);
+    });
+}
+
+bool LegacyDataSPKM::DeleteRecords(WalletBatch& batch)
+{
     LOCK(cs_KeyStore);
-    WalletBatch batch(m_storage.GetDatabase());
     return batch.EraseRecords(DBKeys::LEGACY_TYPES);
 }
 