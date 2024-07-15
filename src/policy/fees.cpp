ismaelsadeeq/bitcoin

[fees]: ignore all transactions that are CPFP'd

src/policy/fees.cpp
@@ -11,6 +11,7 @@
 #include <kernel/mempool_entry.h>
 #include <logging.h>
 #include <policy/feerate.h>
+#include <policy/fees_util.h>
 #include <primitives/transaction.h>
 #include <random.h>
 #include <serialize.h>
@@ -662,6 +663,19 @@ bool CBlockPolicyEstimator::processBlockTx(unsigned int nBlockHeight, const Remo
     return true;
 }
 
+void CBlockPolicyEstimator::removeCPFPdParentTxs(const std::vector<RemovedMempoolTransactionInfo>& txs_removed_for_block)
+{
+    const auto linearizedTransactions = LinearizeTransactions(txs_removed_for_block);
+    std::set<Txid> seen_transactions;
+    for (const auto& tx : txs_removed_for_block) {
+        const auto tx_inclusion_order = linearizedTransactions.inclusion_order.find(tx.info.m_tx->GetHash())->second;
+        const auto mining_fee_rate = std::get<0>(linearizedTransactions.size_per_feerate[tx_inclusion_order]);
+        if (mining_fee_rate != CFeeRate(tx.info.m_fee, tx.info.m_virtual_transaction_size)) {
+            // Ignore all transactions whose mining score is not the same with it's fee rate
+            _removeTx(tx.info.m_tx->GetHash(), /*inBlock=*/true);
+        }
+    }
+}
 void CBlockPolicyEstimator::processBlock(const std::vector<RemovedMempoolTransactionInfo>& txs_removed_for_block,
                                          unsigned int nBlockHeight)
 {
@@ -689,6 +703,12 @@ void CBlockPolicyEstimator::processBlock(const std::vector<RemovedMempoolTransac
     feeStats->UpdateMovingAverages();
     shortStats->UpdateMovingAverages();
     longStats->UpdateMovingAverages();
+
+    // We only consider parent transactions that are mined
+    // solely by their own fee rate. All transactions that are
+    // CPFP'd by some child should be ignored. Child transactions
+    // are not tracked upon entry into the mempool.
+    removeCPFPdParentTxs(txs_removed_for_block);
 
     unsigned int countedTxs = 0;
     // Update averages with data points from current block

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

src/policy/fees.cpp
@@ -10,7 +10,9 @@
 #include <consensus/amount.h>
 #include <kernel/mempool_entry.h>
 #include <logging.h>
+#include <node/mini_miner.h>
 #include <policy/feerate.h>
+#include <policy/fees_util.h>
 #include <primitives/transaction.h>
 #include <random.h>
 #include <serialize.h>
@@ -662,6 +664,20 @@ bool CBlockPolicyEstimator::processBlockTx(unsigned int nBlockHeight, const Remo
     return true;
 }
 
+void CBlockPolicyEstimator::removeCPFPdParentTxs(const std::vector<RemovedMempoolTransactionInfo>& txs_removed_for_block)
+{
+    const auto mini_miner_input = GetMiniMinerInput(txs_removed_for_block);
+    const auto linearizedTransactions = node::MiniMiner(std::get<0>(mini_miner_input), std::get<1>(mini_miner_input)).Linearize();
+    std::set<Txid> seen_transactions;
+    for (const auto& tx : txs_removed_for_block) {
+        const auto tx_inclusion_order = linearizedTransactions.inclusion_order.find(tx.info.m_tx->GetHash())->second;
+        const auto mining_fee_rate = std::get<0>(linearizedTransactions.size_per_feerate[tx_inclusion_order]);
+        if (mining_fee_rate != CFeeRate(tx.info.m_fee, tx.info.m_virtual_transaction_size)) {
+            // Ignore all transactions whose mining score is not the same with it's fee rate
+            _removeTx(tx.info.m_tx->GetHash(), /*inBlock=*/true);
+        }
+    }
+}
 void CBlockPolicyEstimator::processBlock(const std::vector<RemovedMempoolTransactionInfo>& txs_removed_for_block,
                                          unsigned int nBlockHeight)
 {
@@ -689,6 +705,12 @@ void CBlockPolicyEstimator::processBlock(const std::vector<RemovedMempoolTransac
     feeStats->UpdateMovingAverages();
     shortStats->UpdateMovingAverages();
     longStats->UpdateMovingAverages();
+
+    // We only consider parent transactions that are mined
+    // solely by their own fee rate. All transactions that are
+    // CPFP'd by some child should be ignored. Child transactions
+    // are not tracked upon entry into the mempool.
+    removeCPFPdParentTxs(txs_removed_for_block);
 
     unsigned int countedTxs = 0;
     // Update averages with data points from current block