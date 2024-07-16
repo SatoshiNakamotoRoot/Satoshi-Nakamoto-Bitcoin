21d031e7867ef97c07cee691c115d59b3aebfd19
josibake <josibake@protonmail.com>
2024-07-16 18:36:41 +0200

    [staging] refactor: use KeyPair in SignSchnorr
    Use `KeyPair` instead of creating a `secp256k1_keypair` object. The
    main change here is creating a `KeyPair` instead of a
    `secp256k1_keypair` and then passing it to the libsec256k1 functions
    using `reinterpret_cast<secp256k1_keypair*>(keypair)`.
    
    The variable name `m_keypair` is used here to simplify the diff in a
    later commit when all of the logic in SignSchnorr is moved into the
    KeyPair class, where `m_keypair` is then a private member.
    
    Note: this is why `data()` is currently non-const. Since SignSchnorr
    does not have access to the private `m_keypair` member directly, we are cheating
    and accessing it through `data()` in this commit. This is to keep the
    changes incremental and easier to review.

--- src/key.cpp
+++ src/key.cpp
@@ -273,22 +273,22 @@ bool CKey::SignSchnorr(const uint256& hash, Span<unsigned char> sig, const uint2
 {
     assert(sig.size() == 64);
     bool ret = 1;
-    secp256k1_keypair keypair;
-    if (!secp256k1_keypair_create(secp256k1_context_sign, &keypair, UCharCast(begin()))) return false;
+    KeyPair keypair = ComputeKeyPair();
+    if (!keypair.IsValid()) return false;
     if (merkle_root) {
         secp256k1_xonly_pubkey pubkey;
-        ret &= secp256k1_keypair_xonly_pub(secp256k1_context_sign, &pubkey, nullptr, &keypair);
+        ret &= secp256k1_keypair_xonly_pub(secp256k1_context_sign, &pubkey, nullptr, reinterpret_cast<const secp256k1_keypair*>(keypair.data()));
         unsigned char pubkey_bytes[32];
         ret &= secp256k1_xonly_pubkey_serialize(secp256k1_context_sign, pubkey_bytes, &pubkey);
         uint256 tweak = XOnlyPubKey(pubkey_bytes).ComputeTapTweakHash(merkle_root->IsNull() ? nullptr : merkle_root);
-        ret &= secp256k1_keypair_xonly_tweak_add(secp256k1_context_static, &keypair, tweak.data());
+        ret &= secp256k1_keypair_xonly_tweak_add(secp256k1_context_static, reinterpret_cast<secp256k1_keypair*>(keypair.data()), tweak.data());
     }
     if (!ret) return false;
-    ret &= secp256k1_schnorrsig_sign32(secp256k1_context_sign, sig.data(), hash.data(), &keypair, aux.data());
+    ret &= secp256k1_schnorrsig_sign32(secp256k1_context_sign, sig.data(), hash.data(), reinterpret_cast<const secp256k1_keypair*>(keypair.data()), aux.data());
     if (ret) {
         // Additional verification step to prevent using a potentially corrupted signature
         secp256k1_xonly_pubkey pubkey_verify;
-        ret &= secp256k1_keypair_xonly_pub(secp256k1_context_static, &pubkey_verify, nullptr, &keypair);
+        ret &= secp256k1_keypair_xonly_pub(secp256k1_context_static, &pubkey_verify, nullptr, reinterpret_cast<const secp256k1_keypair*>(keypair.data()));
         ret &= secp256k1_schnorrsig_verify(secp256k1_context_static, sig.data(), hash.begin(), 32, &pubkey_verify);
     }
     if (!ret) memory_cleanse(sig.data(), sig.size());
