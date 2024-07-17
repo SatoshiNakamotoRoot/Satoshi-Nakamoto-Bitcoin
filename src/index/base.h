maxvette402/bitcoin

index: Check availability of all necessary data for indices

src/index/base.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_INDEX_BASE_H
 #define BITCOIN_INDEX_BASE_H
 
+#include <chain.h>
 #include <dbwrapper.h>
 #include <interfaces/chain.h>
 #include <util/threadinterrupt.h>
@@ -160,6 +161,9 @@ public:
 
     /// Get a summary of the index and its state.
     IndexSummary GetSummary() const;
+
+    /// Data needed in blocks in order for the index to be able to sync
+    virtual uint32_t NeededBlockData() const = 0;
 };
 
 #endif // BITCOIN_INDEX_BASE_H

src/index/blockfilterindex.h
@@ -75,6 +75,9 @@ public:
     /** Get a range of filter hashes between two heights on a chain. */
     bool LookupFilterHashRange(int start_height, const CBlockIndex* stop_index,
                                std::vector<uint256>& hashes_out) const;
+
+    /** Data needed in blocks in order for the index to be able to sync */
+    uint32_t NeededBlockData() const override {return (BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO);};
 };
 
 /**

src/index/coinstatsindex.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_INDEX_COINSTATSINDEX_H
 #define BITCOIN_INDEX_COINSTATSINDEX_H
 
+#include <chain.h>
 #include <crypto/muhash.h>
 #include <index/base.h>
 
@@ -59,6 +60,9 @@ public:
 
     // Look up stats for a specific block using CBlockIndex
     std::optional<kernel::CCoinsStats> LookUpStats(const CBlockIndex& block_index) const;
+
+    // Data needed in blocks in order for the index to be able to sync
+    uint32_t NeededBlockData() const override {return (BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO);};
 };
 
 /// The global UTXO set hash object.

src/index/txindex.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_INDEX_TXINDEX_H
 #define BITCOIN_INDEX_TXINDEX_H
 
+#include <chain.h>
 #include <index/base.h>
 
 static constexpr bool DEFAULT_TXINDEX{false};
@@ -43,6 +44,9 @@ public:
     /// @param[out]  tx  The transaction itself.
     /// @return  true if transaction is found, false otherwise
     bool FindTx(const uint256& tx_hash, uint256& block_hash, CTransactionRef& tx) const;
+
+    /// Data needed in blocks in order for the index to be able to sync
+    uint32_t NeededBlockData() const override {return BLOCK_HAVE_DATA;};
 };
 
 /// The global transaction index, used in GetTransaction. May be null.

src/test/blockmanager_tests.cpp
@@ -130,6 +130,13 @@ BOOST_FIXTURE_TEST_CASE(blockmanager_block_data_availability, TestChain100Setup)
     BOOST_CHECK_EQUAL(blockman.GetFirstStoredBlock(tip), first_available_block);
     BOOST_CHECK(blockman.CheckBlockDataAvailability(tip, *first_available_block));
     BOOST_CHECK(!blockman.CheckBlockDataAvailability(tip, *last_pruned_block));
+    // Simulate that the first available block is missing undo data and
+    // detect this by using a status mask.
+    first_available_block->nStatus &= ~BLOCK_HAVE_UNDO;
+    BOOST_CHECK_EQUAL(blockman.GetFirstStoredBlock(tip, nullptr, BLOCK_HAVE_DATA), first_available_block);
+    BOOST_CHECK_NE(blockman.GetFirstStoredBlock(tip, nullptr, BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO), first_available_block);
+    BOOST_CHECK(blockman.CheckBlockDataAvailability(tip, *first_available_block, BLOCK_HAVE_DATA));
+    BOOST_CHECK(!blockman.CheckBlockDataAvailability(tip, *first_available_block, BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO));
 }
 
 BOOST_AUTO_TEST_CASE(blockmanager_flush_block_file)

SatoshiNakamotoRoot/Satoshi-Nakamoto-Bitcoin

src/index/base.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_INDEX_BASE_H
 #define BITCOIN_INDEX_BASE_H
 
+#include <chain.h>
 #include <dbwrapper.h>
 #include <interfaces/chain.h>
 #include <util/threadinterrupt.h>
@@ -160,6 +161,9 @@ public:
 
     /// Get a summary of the index and its state.
     IndexSummary GetSummary() const;
+
+    /// Data needed in blocks in order for the index to be able to sync
+    virtual uint32_t RequiredBlockStatusMask() const = 0;
 };
 
 #endif // BITCOIN_INDEX_BASE_H

src/index/blockfilterindex.h
@@ -82,6 +82,9 @@ public:
     /** Get a range of filter hashes between two heights on a chain. */
     bool LookupFilterHashRange(int start_height, const CBlockIndex* stop_index,
                                std::vector<uint256>& hashes_out) const;
+
+    /** Data needed in blocks in order for the index to be able to sync */
+    uint32_t RequiredBlockStatusMask() const override {return (BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO);};
 };
 
 /**

src/index/coinstatsindex.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_INDEX_COINSTATSINDEX_H
 #define BITCOIN_INDEX_COINSTATSINDEX_H
 
+#include <chain.h>
 #include <crypto/muhash.h>
 #include <index/base.h>
 
@@ -59,6 +60,9 @@ public:
 
     // Look up stats for a specific block using CBlockIndex
     std::optional<kernel::CCoinsStats> LookUpStats(const CBlockIndex& block_index) const;
+
+    // Data needed in blocks in order for the index to be able to sync
+    uint32_t RequiredBlockStatusMask() const override {return (BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO);};
 };
 
 /// The global UTXO set hash object.

src/index/txindex.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_INDEX_TXINDEX_H
 #define BITCOIN_INDEX_TXINDEX_H
 
+#include <chain.h>
 #include <index/base.h>
 
 static constexpr bool DEFAULT_TXINDEX{false};
@@ -43,6 +44,9 @@ public:
     /// @param[out]  tx  The transaction itself.
     /// @return  true if transaction is found, false otherwise
     bool FindTx(const uint256& tx_hash, uint256& block_hash, CTransactionRef& tx) const;
+
+    /// Data needed in blocks in order for the index to be able to sync
+    uint32_t RequiredBlockStatusMask() const override {return BLOCK_HAVE_DATA;};
 };
 
 /// The global transaction index, used in GetTransaction. May be null.

src/test/blockmanager_tests.cpp
@@ -131,6 +131,13 @@ BOOST_FIXTURE_TEST_CASE(blockmanager_block_data_availability, TestChain100Setup)
     BOOST_CHECK_EQUAL(blockman.GetFirstBlock(tip, BLOCK_HAVE_DATA), first_available_block);
     BOOST_CHECK(blockman.CheckBlockDataAvailability(tip, *first_available_block));
     BOOST_CHECK(!blockman.CheckBlockDataAvailability(tip, *last_pruned_block));
+    // Simulate that the first available block is missing undo data and
+    // detect this by using a status mask.
+    first_available_block->nStatus &= ~BLOCK_HAVE_UNDO;
+    BOOST_CHECK_EQUAL(blockman.GetFirstBlock(tip, BLOCK_HAVE_DATA, nullptr), first_available_block);
+    BOOST_CHECK_NE(blockman.GetFirstBlock(tip, BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO, nullptr), first_available_block);
+    BOOST_CHECK(blockman.CheckBlockDataAvailability(tip, *first_available_block, BLOCK_HAVE_DATA));
+    BOOST_CHECK(!blockman.CheckBlockDataAvailability(tip, *first_available_block, BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO));
 }
 
 BOOST_AUTO_TEST_CASE(blockmanager_flush_block_file)